bcb696a251e95754a5b8d344657a2844d8597471
Valdo227 <59632809+Valdo227@users.noreply.github.com>
2021-11-29 01:03:22 -0500

    fear(profile): picture upload added

---
+++ helpers/fileUpload.js
@@ -0,0 +1,39 @@
+const cloudinary = require('cloudinary').v2;
+require('dotenv').config();
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+    secure: true
+});
+
+exports.upload = (file, folder, id) => {
+    let options;
+
+    if (id != null)
+        options = {
+            public_id: id,
+            folder: folder,
+            resource_type: 'auto',
+            overwrite: true,
+        }
+    else
+        options = {
+            folder: folder,
+            resource_type: 'auto'
+        }
+
+    return new Promise(resolve => {
+        cloudinary.uploader.upload(file, options).then(response =>
+            resolve({
+            id: response.public_id,
+            url: response.secure_url
+        }))
+    })
+
+};
+
+exports.delete = (image_id) => {
+    cloudinary.uploader.destroy(image_id)
+}
